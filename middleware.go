9207c29686c85dcb4beeb3050f9e2fc4693b62dc
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2025-05-15 09:56:29 -0500

    Update middleware.go

--- middleware.go
+++ middleware.go
@@ -32,7 +32,7 @@ type tokenStruct struct {
 
 // TODO:
 //	[x] 1. Logging
-//	[] 2. Auth
+//	[x] 2. Auth
 
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
